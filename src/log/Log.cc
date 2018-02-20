d1c1b9fbb3e80c89706a9fec8501a0a3241ca9cf
Mohamad Gebai <mgebai@suse.com>
2018-02-19 22:35:29 -0500

    tracing: replace plain text logging with tracing
    * Add ceph_logging_tracing library
    * Hardcode Log.cc to write a tracepoint instead of plain text
    
    Signed-off-by: Mohamad Gebai <mgebai@suse.com>

--- src/log/Log.cc
+++ src/log/Log.cc
@@ -15,6 +15,7 @@
 #include "include/assert.h"
 #include "include/compat.h"
 #include "include/on_exit.h"
+#include <lttng/lttng.h>
 
 #include "Entry.h"
 #include "SubsystemMap.h"
@@ -24,6 +25,16 @@
 
 #define PREALLOC 1000000
 
+
+#ifdef WITH_LTTNG
+#define TRACEPOINT_DEFINE
+#define TRACEPOINT_PROBE_DYNAMIC_LINKAGE
+#include "tracing/ceph_logging.h"
+#undef TRACEPOINT_PROBE_DYNAMIC_LINKAGE
+#undef TRACEPOINT_DEFINE
+#else
+#define tracepoint(...)
+#endif
 
 namespace ceph {
 namespace logging {
@@ -210,6 +221,11 @@ void Log::stop_graylog()
 
 void Log::submit_entry(Entry *e)
 {
+  if (true) {
+    tracepoint(ceph_logging, log_message, (char*)e->get_str().c_str());
+    return;
+  }
+
   pthread_mutex_lock(&m_queue_mutex);
   m_queue_mutex_holder = pthread_self();
 
