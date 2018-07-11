954fbcb5a174af22692193b4fecd4dcf56e40df5
Mohamad Gebai <mgebai@suse.com>
2018-07-11 22:16:35 +0200

    tracing: rm unused options
    Signed-off-by: Mohamad Gebai <mgebai@suse.com>

--- src/vstart.sh
+++ src/vstart.sh
@@ -487,7 +487,6 @@ $CMGRDEBUG
 $extra_conf
 [osd]
 $DAEMONOPTS
-        osd_logging_tracing = true
         logging legacy = true
         osd_check_max_object_name_len_on_startup = false
         osd data = $CEPH_DEV_DIR/osd\$id
